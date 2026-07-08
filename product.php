615b1ea7d1fbea199bb0526f983a5351eaccd886
dechathorn mingkwan <68dechathon.m@nsru.ac.th>
2026-07-08 14:34:56 +0700

    เพิ่มพนักงาน

--- product.php
+++ product.php
@@ -3,12 +3,23 @@
         Product :<input type="text" name="Product_name"><br>
         Product Price :<input type="text" name="Product_price"><br>
         description :<textarea name="Product_description"> </textarea><br>
-
     <input type="radio" name="Product_Category" value="1">ขนม
     <input type="radio" name="Product_Category" value="2">น้ำดื่ม
     <input type="radio" name="Product_Category" value="3">แปปชี่
     <input type="radio" name="Product_Category" value="4">โค้ก
     <input type="radio" name="Product_Category" value="5">ข้าว<br>
+
+    <input type=checkbox name="Product_Checkbox" value="1">
+       <label for="Product_Checkbox">Retail</label>
+    <input type=checkbox name="Product_Checkbox" value="2">
+       <label for="Product_Checkbox">wholesale</label>
+    <input type=checkbox name="Product_Checkbox" value="3">
+       <label for="Product_Checkbox">both</label><br>
+
+       <input type="radio" name="employee" value="1">boy
+    <input type="radio" name="employee" value="2">nat
+    <input type="radio" name="employee" value="3">dear<br>
+
 
    </form>
 </html>
