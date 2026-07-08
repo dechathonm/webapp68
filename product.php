553d2e0236efdc3621c57cb2e5fe803fd926ac2a
dechathorn mingkwan <68dechathon.m@nsru.ac.th>
2026-07-08 14:17:51 +0700

    เพิ่มสินค้า ราคาสินค้า

---
+++ product.php
@@ -0,0 +1,7 @@
+<html>
+    <form name="frm-register" action="product_result.php" method="POST">
+        Product :<input type="text" name="Product_name"><br>
+        Product Price :<input type="text" name="Product_price"><br>
+        description :<textarea name="Product_description"> </textarea><br>
+   </form>
+</html>
