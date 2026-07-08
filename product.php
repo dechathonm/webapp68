596ffb4041dada85edf6789e40a8f881f83920d4
dechathorn mingkwan <68dechathon.m@nsru.ac.th>
2026-07-08 14:30:31 +0700

    เพิ่ม ประเภทสินค้า

--- product.php
+++ product.php
@@ -3,5 +3,12 @@
         Product :<input type="text" name="Product_name"><br>
         Product Price :<input type="text" name="Product_price"><br>
         description :<textarea name="Product_description"> </textarea><br>
+
+    <input type="radio" name="Product_Category" value="1">ขนม
+    <input type="radio" name="Product_Category" value="2">น้ำดื่ม
+    <input type="radio" name="Product_Category" value="3">แปปชี่
+    <input type="radio" name="Product_Category" value="4">โค้ก
+    <input type="radio" name="Product_Category" value="5">ข้าว<br>
+
    </form>
 </html>
