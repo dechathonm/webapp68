952c4fd193adc801c741b1834055b4303efcfd2a
dechathorn mingkwan <68dachathorn.m@nsru.ac.th>
2026-07-16 11:50:21 +0700

    add showshop shopreslt connectshop

--- shop.php
+++ shop.php
@@ -1,8 +1,8 @@
 <html>
     <form name="frm_register" method="POST" action="register_reslt.php">
-        <input type="text" name="product_name"><br>
-        <input type="text" name="product_price"><br>
-        <input type="text" name="product_brand"><br>
+      product_name  <input type="text" name="product_name"><br>
+        product_price<input type="text" name="product_price"><br>
+        product_brand<input type="text" name="product_brand"><br>
         <input type="submit" name="submit" value="SAVE">
         <input type="reset" name="reset" value="CANCEL">
 </form>
---
+++ show_shop.php
@@ -0,0 +1,30 @@
+<?php
+include 'connectshop.php';
+
+$sql ="SELECT * FROM tb_shop;";
+$result = mysqli_query($link,$sql);
+?>
+<html>
+    <table border="2">
+        <tr>
+            <td>ID</td>
+            <td>NAME</td>
+            <td>E-MAIL</td>
+            <td>PHONE</td>
+        </rt>
+        <?php
+        while($arr = mysqli_fetch_array($result))
+            {
+        ?>
+       <tr>
+            <td><?php echo $arr['product_id']; ?></td>
+            <td><?php echo $arr['product_name']; ?></td>
+            <td><?php echo $arr['product_price']; ?></td>
+            <td><?php echo $arr['product_brand']; ?></td>
+        </tr>
+        <?php
+}
+?>
+    </table>
+
+<html>
